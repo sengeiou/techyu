555e849037138549247a37c29a59250e7578cf48
taewon59yu <trigger7@naver.com>
2021-06-22 23:24:10 +0900

    DJE D

---
+++ src/main/java/kr/co/techyu/TechYuApplication.java
@@ -0,0 +1,13 @@
+package kr.co.techyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechYuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechYuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/techyu/TechYuApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.techyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechYuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
