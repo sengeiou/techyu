39fc82541eaea44e762974fee279fad9b7adbd08
taewon59yu <trigger7@naver.com>
2021-06-23 20:52:41 +0900

    추가

--- src/main/java/kr/co/techyu/TechYuApplication.java
+++ src/main/java/kr/co/techyu/TechYuApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TechYuApplication {
 
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TechYuApplication.class, args);
 	}
