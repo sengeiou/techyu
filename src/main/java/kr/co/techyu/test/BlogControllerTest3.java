d41f85a072e343c3c9f83923550161fb04e65f45
taewon59yu <trigger7@naver.com>
2021-06-25 21:48:08 +0900

    우리것이 좋은것이여~~

---
+++ src/main/java/kr/co/techyu/test/BlogControllerTest3.java
@@ -0,0 +1,27 @@
+package kr.co.techyu.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class BlogControllerTest3 implements WebMvcConfigurer{
+
+	@RequestMapping("/")
+	public String main() {
+		return "index";
+	}
+	
+	@Override
+	public void addResourceHandlers( final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+				.addResourceLocations("classpath:/templates/")
+				.setCacheControl(CacheControl.maxAge(10,TimeUnit.MINUTES));
+				
+	}
+}
