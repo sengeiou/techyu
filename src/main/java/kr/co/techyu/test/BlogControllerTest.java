5a4c032b298dc169125cd4abd83010b04c860c59
kbntt <kbntt73@gmail.com>
2021-06-25 22:37:40 +0900

    BlogControllerTest 수정

--- src/main/java/kr/co/techyu/test/BlogControllerTest.java
+++ src/main/java/kr/co/techyu/test/BlogControllerTest.java
@@ -1,15 +1,37 @@
 package kr.co.techyu.test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
-
-@RestController
+@Controller
 public class BlogControllerTest {
 
+
+	@Autowired
+    private TestSevice testSevice;
+	
 	@GetMapping("/test/hello")
 	public String hello() {
 		return "<h1>XXXX hellow XXXX</h1>";
 	}
+	@GetMapping("/test/memberList")
+	public String memberList(Model model, MemberModel dto, @RequestParam Map<String, Object> param) {
+		
+		HashMap<String, Object> paramMap =  new HashMap<String, Object>();
+		paramMap.put("EMAIL", dto.getEmail());
+		
+		List<MemberModel> memberList = testSevice.memberList(paramMap);
+		model.addAttribute("memberList", memberList);
+		
+		return "index";
+	}
+	
 }
