b2c1d2ebef5a31aacbc8de7fa80057b11ee27d13
taewon59yu <trigger7@naver.com>
2021-07-01 21:53:33 +0900

    삭제

--- src/main/java/kr/co/techyu/test/BlogControllerTest.java
+++ src/main/java/kr/co/techyu/test/BlogControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import kr.co.techyu.model.User;
 
 @Controller
 public class BlogControllerTest {
